12dbc6311772bde7bc2af84fff3c6b29c8e41c6a
Kayano <oyama_kayano@yahoo.co.jp>
2019-04-05 13:09:54 +0900

    Updates

---
+++ precache-manifest.28cbc70ad393f325ebbb29dee68ede03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe35fca0fea852899367",
+    "url": "/react_todo/static/js/runtime~main.db2a12cb.js"
+  },
+  {
+    "revision": "74930b822280f448e33d",
+    "url": "/react_todo/static/js/main.b235dbe5.chunk.js"
+  },
+  {
+    "revision": "8a68af603cd4200d532d",
+    "url": "/react_todo/static/js/2.e4658b1a.chunk.js"
+  },
+  {
+    "revision": "74930b822280f448e33d",
+    "url": "/react_todo/static/css/main.16b3c747.chunk.css"
+  },
+  {
+    "revision": "756a625430ff04c8243cf9448835e141",
+    "url": "/react_todo/index.html"
+  }
+];
